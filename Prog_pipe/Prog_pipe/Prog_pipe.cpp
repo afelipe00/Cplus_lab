7ece6dd1054c7377bcede2e32156385f753073c9
Felipe Díaz <afelipe00@users.noreply.github.com>
2019-05-21 19:55:31 -0500

    comentareado de el parcial

---
+++ Prog_pipe/Prog_pipe/Prog_pipe.cpp
@@ -0,0 +1,117 @@
+// Prog_pipe.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct jugador {
+	string nombre;
+	int coord[2];
+	int lim = 2;
+	int mov = 0;
+
+};
+
+struct laberinto {
+	jugador jug[100000];
+	int numj = 0;
+	int posi[2];
+	int tablero[10000][10000];
+	int tab_tam[2];
+};
+
+void ganador(laberinto tab) {
+	bool fin = false;
+	for (int i = 0; i < tab.numj; i++) {
+		fin = false;
+		cout << "jugador " << i << " : ";
+		do {
+			switch (tab.tablero[tab.jug[i].coord[0]][tab.jug[i].coord[1]]) {
+			case 0:
+				fin = true;
+				break;
+			case 1:
+				tab.jug[i].coord[0] = tab.jug[i].coord[0] - 1;
+				tab.jug[i].mov++;
+				cout << " arriba" << endl;
+				break;
+			case 2:
+				tab.jug[i].coord[0] = tab.jug[i].coord[0] - 1;
+				tab.jug[i].coord[1] = tab.jug[i].coord[1] + 1;
+				tab.jug[i].mov += 2;
+				cout << " arriba derecha" << endl;
+				break;
+			case 3:
+				tab.jug[i].coord[1] = tab.jug[i].coord[1] + 1;
+				tab.jug[i].mov++;
+				cout << " derecha" << endl;
+				break;
+			case 4:
+				tab.jug[i].coord[0] = tab.jug[i].coord[0] + 1;
+				tab.jug[i].coord[1] = tab.jug[i].coord[1] + 1;
+				tab.jug[i].mov += 2;
+				cout << " abajo derecha" << endl;
+				break;
+			case 5:
+				tab.jug[i].coord[0] = tab.jug[i].coord[0] + 1;
+				tab.jug[i].mov++;
+				cout << " abajo " << endl;
+				break;
+			case 6:
+				tab.jug[i].coord[0] = tab.jug[i].coord[0] + 1;
+				tab.jug[i].coord[1] = tab.jug[i].coord[1] - 1;
+				tab.jug[i].mov += 2;
+				cout << " abajo izquierda" << endl;
+				break;
+			case 7:
+				tab.jug[i].coord[1] = tab.jug[i].coord[1] - 1;
+				tab.jug[i].mov++;
+				cout << " izquierda " << endl;
+				break;
+			case 8:
+				tab.jug[i].coord[0] = tab.jug[i].coord[0] - 1;
+				tab.jug[i].coord[1] = tab.jug[i].coord[1] - 1;
+				tab.jug[i].mov += 2;
+				cout << " arriba izquierda" << endl;
+				break;
+			default:
+				break;
+			}
+			if (tab.jug[i].coord[0] == tab.posi[0] && tab.jug[i].coord[1] == tab.posi[1]) {
+				fin = true;
+			}
+			if (tab.jug[i].coord[0] > tab.tab_tam[0] || tab.jug[i].coord[1] > tab.tab_tam[1]) {
+				fin = true;
+			}
+			cout << "\nel numero de movimientos fue: " << tab.jug[i].mov;
+		} while (fin == false);
+	}
+	int menor = tab.jug[0].mov;
+	int pos_g = 0;
+	for (int i = 0; i < tab.numj; i++) {
+		if (tab.jug[i].mov < menor) {
+			menor = tab.jug[i].mov;
+			pos_g = i;
+		}
+	}
+	cout << "Nombre Ganador: " << tab.jug[pos_g].nombre;
+	cout << "\nmovimientos: " << menor;
+}
+
+int main()
+{
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
--- pruebacod/pruebacod/pruebacod.cpp
+++ pruebacod/pruebacod/pruebacod.cpp
@@ -1,14 +1,84 @@
-// pruebacod.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// pruebacod.cpp : Este archivo contiene la funci�n "main". La ejecuci�n del programa comienza y termina ah�.
 //
 
 #include"pch.h"
 //librerias de c++
-# include <iostream>
+//libreria para windows
+#include<iostream>
+//libreria que maneja propiedades del string
 #include <string>
+//libreria estandar c
+#include <stdlib.h>
+//libreria pra utilizar el setw
+#include<iomanip>
 
 using namespace std;
 
+struct coordenada {
+	int pos[2];
+	int lim = 2;
+};
+
+struct Poligono {
+	int resolucion = 0;
+	coordenada vertices[10000];
+	string color;
+	int perimetro = 0;
+};
+
+void perimetro(Poligono pol) {
+	int y = 0, int x = 0; int res = 0;
+	for (int i = 0; i < pol.resolucion - 1; i++) {
+		if (pol.vertices[i].pos[0] == pol.vertices[i + 1].pos[0]) {
+			res = (pol.vertices[i + 1].pos[1] - pol.vertices[i].pos[1]);
+			if (res < 0) {
+				res = res * (-1);
+			}
+			y = y + res; res = 0;
+		}
+		if (pol.vertices[i].pos[1] == pol.vertices[i + 1].pos[1]) {
+			res = (pol.vertices[i + 1].pos[0] - pol.vertices[i].pos[0]);
+			if (res < 0) {
+				res = res * (-1);
+			}
+			x = x + res; res = 0;
+		}
+	}
+	pol.perimetro = x + y;
+}
+
+void conjunto(Poligono pol[], int tam) {
+	Poligono temp;
+	int menor, mayor;
+	for (int i = 0; i < tam; i++) {
+		for (int j = 0; j < tam - 1; j++) {
+			if (pol[j].resolucion < pol[j + 1].resolucion) {
+				temp = pol[j];
+				pol[j] = pol[j + 1];
+				pol[j + 1] = temp;
+			}
+		}
+	}
+	cout << "poligonos con mayor resolucion: " << endl;
+	mayor = pol[0].resolucion;
+	for (int i = 0; i < tam; i++) {
+		if (pol[i].resolucion >= mayor) {
+			cout << "poligono " << i << " " << "resolucion: " << pol[i].resolucion << endl;
+			mayor = pol[i].resolucion;
+		}
+	}
+	cout << "poligonos con menor perimetro: " << endl;
+	menor = pol[0].perimetro;
+	for (int i = 0; i < tam; i++) {
+		if (pol[i].perimetro <= menor) {
+			cout << "poligono " << i << " " << "perimetro: " << pol[i].perimetro << endl;
+			menor = pol[i].perimetro;
+		}
+	}
+
+}
 
 int main(void) {
+
 
 }
