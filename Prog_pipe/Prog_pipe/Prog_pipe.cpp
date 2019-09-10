83d0e9531958e1529eed03d07429d1fd84680d94
Felipe Díaz <afelipe00@users.noreply.github.com>
2019-09-09 23:19:13 -0500

    sdds

--- Prog_pipe/Prog_pipe/Prog_pipe.cpp
+++ Prog_pipe/Prog_pipe/Prog_pipe.cpp
@@ -7,111 +7,58 @@
 
 using namespace std;
 
-struct jugador {
-	string nombre;
-	int coord[2];
-	int lim = 2;
-	int mov = 0;
+int main(void) {
+	int vehiculo, peajes, costo,total;
 
-};
+	cout << "digite el tipo de vehiculo";
+	cin >> vehiculo;
+	cout << "Digite los peajes";
+	cin >> peajes;
 
-struct laberinto {
-	jugador jug[100000];
-	int numj = 0;
-	int posi[2];
-	int tablero[10000][10000];
-	int tab_tam[2];
-};
+	switch (vehiculo) {
+	case 0: 
+		costo = 0;
+		break;
+	case 1: 
+		costo = 10500;
+		break;
+	case 2: 
+		costo = 18700;
+		break;
+	case 3: 
+		costo = 25400;
+		break;
+	case 4:
+		costo = 36200;
+		break;
+	default: 
+		cout << "carro no existente";
 
-void ganador(laberinto tab) {
-	bool fin = false;
-	for (int i = 0; i < tab.numj; i++) {
-		fin = false;
-		cout << "jugador " << i << " : ";
-		do {
-			switch (tab.tablero[tab.jug[i].coord[0]][tab.jug[i].coord[1]]) {
-			case 0:
-				fin = true;
-				break;
-			case 1:
-				tab.jug[i].coord[0] = tab.jug[i].coord[0] - 1;
-				tab.jug[i].mov++;
-				cout << " arriba" << endl;
-				break;
-			case 2:
-				tab.jug[i].coord[0] = tab.jug[i].coord[0] - 1;
-				tab.jug[i].coord[1] = tab.jug[i].coord[1] + 1;
-				tab.jug[i].mov += 2;
-				cout << " arriba derecha" << endl;
-				break;
-			case 3:
-				tab.jug[i].coord[1] = tab.jug[i].coord[1] + 1;
-				tab.jug[i].mov++;
-				cout << " derecha" << endl;
-				break;
-			case 4:
-				tab.jug[i].coord[0] = tab.jug[i].coord[0] + 1;
-				tab.jug[i].coord[1] = tab.jug[i].coord[1] + 1;
-				tab.jug[i].mov += 2;
-				cout << " abajo derecha" << endl;
-				break;
-			case 5:
-				tab.jug[i].coord[0] = tab.jug[i].coord[0] + 1;
-				tab.jug[i].mov++;
-				cout << " abajo " << endl;
-				break;
-			case 6:
-				tab.jug[i].coord[0] = tab.jug[i].coord[0] + 1;
-				tab.jug[i].coord[1] = tab.jug[i].coord[1] - 1;
-				tab.jug[i].mov += 2;
-				cout << " abajo izquierda" << endl;
-				break;
-			case 7:
-				tab.jug[i].coord[1] = tab.jug[i].coord[1] - 1;
-				tab.jug[i].mov++;
-				cout << " izquierda " << endl;
-				break;
-			case 8:
-				tab.jug[i].coord[0] = tab.jug[i].coord[0] - 1;
-				tab.jug[i].coord[1] = tab.jug[i].coord[1] - 1;
-				tab.jug[i].mov += 2;
-				cout << " arriba izquierda" << endl;
-				break;
-			default:
-				break;
-			}
-			if (tab.jug[i].coord[0] == tab.posi[0] && tab.jug[i].coord[1] == tab.posi[1]) {
-				fin = true;
-			}
-			if (tab.jug[i].coord[0] > tab.tab_tam[0] || tab.jug[i].coord[1] > tab.tab_tam[1]) {
-				fin = true;
-			}
-			cout << "\nel numero de movimientos fue: " << tab.jug[i].mov;
-		} while (fin == false);
 	}
-	int menor = tab.jug[0].mov;
-	int pos_g = 0;
-	for (int i = 0; i < tab.numj; i++) {
-		if (tab.jug[i].mov < menor) {
-			menor = tab.jug[i].mov;
-			pos_g = i;
-		}
+	total = costo * peajes;
+
+	if (peajes < 2) {
+		costo = costo;
 	}
-	cout << "Nombre Ganador: " << tab.jug[pos_g].nombre;
-	cout << "\nmovimientos: " << menor;
+	else if (peajes == 2) {
+		costo = costo -(costo * 0.5);
+	}
+	else if (peajes == 3) {
+		costo = costo - (costo * 0.10);
+	}
+	else if (peajes >= 4) {
+		costo = costo - (costo * 0.20);
+
+	}
+
+	if (total > 110000) {
+		total = total - (total * 0.90);
+	}
+
+
+	cout << "El costo total es:" << total << endl;
+
+
+
+
 }
-
-int main()
-{
-	cout << "asdfasdfasdf";
-}
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
