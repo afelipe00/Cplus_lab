3ac7c7211d1c3debf1392da9bbb4bb10c4f01594
Felipe Díaz <afelipe00@users.noreply.github.com>
2019-10-08 19:14:53 -0500

    taller 2do corte cliente 17

--- Prog_pipe/Prog_pipe/Prog_pipe.cpp
+++ Prog_pipe/Prog_pipe/Prog_pipe.cpp
@@ -1,64 +1,79 @@
 // Prog_pipe.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
-
 #include "pch.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+int salto_correcto(int vect[], int tam) {
+	int indice = 0;
+	int pasos = 0;
+	bool band = true;
+	int temp[] = { 0,0,0,0,0,0,0,0,0,0 };
+	for (int i = 0; i < tam; i++) {
+		temp[i] = vect[i];
+	}
+	do {
+		if (temp[indice] != 0) {
+			if (indice + temp[indice] <= tam) {
+				pasos = pasos + temp[indice];
+				indice = indice + temp[indice];
+				temp[indice - temp[indice]] = 0;
+			}
+			else
+			{
+				if(indice < tam) {
+					int tempi = indice;
+					pasos = pasos + temp[indice];
+					indice = (temp[indice]-(tam+indice))+2;
+					temp[tempi] = 0;
+				}
+				else  if (indice = tam) {
+					int tempi = indice;
+					pasos = pasos + temp[indice];
+					indice = (temp[indice] - (tam + indice)) + 1;
+					temp[tempi] = 0;
+				}
+			}
+		}
+		else
+		{
+			band = false;
+			break;
+		}
+	} while (indice != 0);
+	if (band == true) {
+		return pasos;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+void ganadora(int vec1[], int tam1, int vec2[], int tam2) {
+	int saltos1 = salto_correcto(vec1, tam1);
+	int saltos2 = salto_correcto(vec2, tam2);
+	if (saltos1 == -1) {
+		cout << "la ranita 1 tiene salto incorrectos" << endl;
+	}
+	if (saltos2 == -1) {
+		cout << "la ranita 2 tiene salto incorrectos" << endl;
+	}
+	if (saltos1 > saltos2) {
+		cout << "Gano la ranita 1" << endl;
+	}
+	else if (saltos2 > saltos1) {
+		cout << "Gano la ranita 2" << endl;
+	}
+	else {
+		cout << "empate" << endl;
+	}
+}
+
 int main(void) {
-	int vehiculo, peajes, costo,total;
-
-	cout << "digite el tipo de vehiculo";
-	cin >> vehiculo;
-	cout << "Digite los peajes";
-	cin >> peajes;
-
-	switch (vehiculo) {
-	case 0: 
-		costo = 0;
-		break;
-	case 1: 
-		costo = 10500;
-		break;
-	case 2: 
-		costo = 18700;
-		break;
-	case 3: 
-		costo = 25400;
-		break;
-	case 4:
-		costo = 36200;
-		break;
-	default: 
-		cout << "carro no existente";
-
-	}
-	total = costo * peajes;
-
-	if (peajes < 2) {
-		costo = costo;
-	}
-	else if (peajes == 2) {
-		costo = costo -(costo * 0.5);
-	}
-	else if (peajes == 3) {
-		costo = costo - (costo * 0.10);
-	}
-	else if (peajes >= 4) {
-		costo = costo - (costo * 0.20);
-
-	}
-
-	if (total > 110000) {
-		total = total - (total * 0.90);
-	}
-
-
-	cout << "El costo total es:" << total << endl;
-
-
-
-
+	int vect[] = { 3,6,2,8,1 };
+	int tam = 5;
+	int resp = salto_correcto(vect, tam);
 }
